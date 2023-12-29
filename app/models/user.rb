ea11a5f612d73518a86a026982b20d0d7bd6fb32
kyoroma <138451440+kyoroma@users.noreply.github.com>
2023-12-29 18:59:42 +0900

    Usreモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,27 +13,12 @@ class User < ApplicationRecord
 
   attribute :is_active, :boolean, default: true
 
-  #def guest?
-    #email == 'guest@example.com'
-  #end
-
   # ゲストユーザーを作成するメソッド
   def self.guest
     find_or_create_by(email: 'guest@example.com') do |user|
       user.name = 'Guest User'
       user.password = Devise.friendly_token[0, 20]
     end
-
-    #guest_user.assign_attributes(guest: true)
-
-    #if guest_user.save
-      #guest_user
-    #else
-      # ゲストユーザーの作成に失敗した場合の処理を追加
-      # 例えばエラーログを記録したり、特定の処理を行ったりします
-      #Rails.logger.error("Failed to create guest user: #{guest_user.errors.full_messages}")
-      #nil
-    #end
   end
 
   def active_for_authentication?
